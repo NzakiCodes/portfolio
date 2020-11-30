e019240008d82ea6f2735322582b50e0dcb525ba
Nzaki Michael Ekere <ekisnzaki@gmail.com>
2020-11-30 10:56:06 +0100

    Added animation to title

--- pages/about.js
+++ pages/about.js
@@ -9,24 +9,36 @@ export default function Home() {
             <Title>About</Title>
             <div className={util.About__header}>
                 <motion.h1
-                    initial={{ 
+                    initial={{
                         opacity: 0,
-                        x:-30,
-                        y:-30,
+                        x: -30,
+                        y: -30,
                         letterSpacing: '-20px'
-                     }}
-                    animate={{ 
+                    }}
+                    animate={{
                         opacity: 1,
-                        x:0,
-                        y:0,
+                        x: 0,
+                        y: 0,
                         letterSpacing: '5px'
 
-                     }}
-                    
-                    transition={{ duration: 0.8,ease:"easeOut" }}
-                >Nzaki <span>Michael</span>
+                    }}
+
+                    transition={{ duration: 0.8, ease: "easeOut" }}
+                >
+                    Nzaki <span>Michael</span>
                     <span className={util.pinkDot}></span>
                 </motion.h1>
+                <motion.h3 initial={{
+                        opacity: 0,
+                        letterSpacing: '-20px'
+                    }}
+                    animate={{
+                        opacity: 1,
+                        letterSpacing: '5px'
+
+                    }}
+
+                    transition={{ duration: 0.8, ease: "easeOut" }}>FullStack Web Developer</motion.h3>
             </div>
         </Layout>
     )
