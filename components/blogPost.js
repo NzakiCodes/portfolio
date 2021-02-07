93eacfa615ccf5c76433ae887c97461132fb4689
Nzaki Michael Ekere <ekisnzaki@gmail.com>
2021-02-07 19:54:49 +0100

    Added Animation to projects

--- components/blogPost.js
+++ components/blogPost.js
@@ -1,6 +1,8 @@
 import { gql, useQuery } from '@apollo/client';
+import { motion } from 'framer-motion';
 import { PostCard } from "../components/cards";
 import styles from '../styles/util.module.css';
+
 const BLOG_POST = gql`  query{
     user(username:"NzakiCodes"){
     publicationDomain
@@ -18,16 +20,36 @@ const BLOG_POST = gql`  query{
 
 const BlogPosts = () => {
     const { loading, data, error } = useQuery(BLOG_POST);
+    const container = {
+        hidden: { opacity: 1, scale: 0 },
+        visible: {
+            opacity: 1,
+            scale: 1,
+            transition: {
+                delayChildren: 0.3,
+                staggerChildren: 0.2
+            }
+        }
+    }
+
+    const item = {
+        hidden: { y: 20, opacity: 0 },
+        visible: {
+            y: 0,
+            opacity: 1
+        }
+    }
     return (
-        <div className={styles.flexRow}>
+        <motion.div variants={container} initial="hidden" animate="visible" className={styles.flexRow}>
             {
                 loading ? <h1>Loading...</h1> : (error ? <h1>Error!!!</h1> :
                     data.user.publication.posts.map((post) => (
-
-                        <PostCard key={post._id} post={post} url={data.user.publicationDomain} />
+                        <motion.div className="item" variants={item} key={post._id} >
+                            <PostCard post={post} url={data.user.publicationDomain} />
+                        </motion.div>
                     )))
             }
-        </div>
+        </motion.div>
     )
 
 }
--- pages/projects.js
+++ pages/projects.js
@@ -2,22 +2,84 @@ import { ImageCard } from '../components/cards';
 import { Title } from '../components/typography';
 import Layout from '../components/layout';
 import styles from '../styles/util.module.css';
+import { motion } from 'framer-motion';
 
 export default function Projects() {
+  const projects = [
+    {
+      id: 1,
+      src: "/images/projects/feyrep.png"
+    },
+    {
+      id: 2,
+      src: "/images/projects/NTASA.png"
+    },
+    {
+      id: 3,
+      src: "/images/projects/pearla_confectioneries.jpg"
+    },
+    {
+      id: 4,
+      src: "/images/projects/breakingbad.png"
+    },
+    {
+      id: 5,
+      src: "/images/projects/trueworship-app.png"
+    },
+    {
+      id: 6,
+      src: "/images/projects/tycoone-tk.png"
+    },
+    {
+      id: 7,
+      src: "/images/projects/music-go.png"
+    },
+    {
+      id: 8,
+      src: "/images/projects/salesforce-x.png"
+    },
+    {
+      id: 9,
+      src: "/images/projects/breakingbad.png"
+    },
+    {
+      id: 10,
+      src: "/images/projects/book-store.png"
+    }
+  ];
+  const container = {
+    hidden: { opacity: 1, scale: 0 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        delayChildren: 0.3,
+        staggerChildren: 0.2
+      }
+    }
+  };
+
+  const item = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1
+    }
+  };
   return (
     <Layout pageTitle="Projects" navActive="projects">
       <Title>Projects</Title>
-      <div className={styles.flexRow}>
-        <ImageCard animate image="/images/projects/feyrep.png" />
-        <ImageCard image="/images/projects/NTASA.png" />
-        <ImageCard image="/images/projects/pearla_confectioneries.jpg" />
-        <ImageCard image="/images/projects/breakingbad.png" />
-        <ImageCard image="/images/projects/trueworship-app.png" />
-        <ImageCard image="/images/projects/tycoone-tk.png" />
-        <ImageCard image="/images/projects/music-go.png" />
-        <ImageCard image="/images/projects/salesforce-x.png" />
-        <ImageCard image="/images/projects/book-store.png" />
-      </div>
+      <motion.div variants={container}
+        initial="hidden"
+        animate="visible" className={styles.flexRow}>
+        {
+          projects.map((project) => (
+            <motion.div key={project.id} variants={item} >
+              <ImageCard variants={item} image={project.src} />
+            </motion.div>
+          ))
+        }
+      </motion.div>
     </Layout>
   )
 }
